871575ac5b39218da6e01a3d8304c97f757e86f0
Lidia Yanes Garcia <35604641+lyanesgarcia@users.noreply.github.com>
2020-07-19 17:19:04 +0100

    parser

---
+++ parser(Harvard)/parser.py
@@ -0,0 +1,93 @@
+import nltk
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to" | "until"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S -> NP VP 
+VP -> V | V NP | V P | V Adv 
+NP -> N | N VP | Det NP | N Conj | N Adv | N P
+P -> P Det | P NP
+Adj -> Adj NP | Adj Adj
+Det -> Det Adj | Det N
+Adv -> Adv Conj
+Conj -> Conj VP | Conj NP 
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Sentence: ")
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+def judge(word):
+    flag = False
+    for s in word:
+        if s.isalpha():
+            flag = True
+            break
+    
+    return flag
+    
+def preprocess(sentence):
+    """
+    Convert `sentence` to a list of its words.
+    Pre-process sentence by converting all characters to lowercase
+    and removing any word that does not contain at least one alphabetic
+    character.
+    """
+    words = nltk.wordpunct_tokenize(sentence)
+    res = []
+    for word in words:
+        word_lower = word.lower()
+        if judge(word_lower):
+            res.append(word_lower)
+    return res
+    #raise NotImplementedError
+    
+
+
+
+if __name__ == "__main__":
+    main()
